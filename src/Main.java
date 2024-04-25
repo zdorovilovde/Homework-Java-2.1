35f43b26504b694b0e4db0ef0a43489a6e65ed34
zdorovilov test <zdorovilovde@mail.ru>
2024-04-25 22:18:12 +0500

    Update src/Main.Java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,26 @@
 public class Main {
-    
+    public static void main(String[] args) {
+
+        int initialScore = 100;
+        int replenishmentAmount = 1100;
+        int bonus = replenishmentAmount / 100;
+        System.out.println("Итоговая сумма: " + (initialScore + replenishmentAmount));
+        if (replenishmentAmount > 1000) {
+            System.out.println("Бонус равен:" + bonus);
+        } else {
+            System.out.println("Бонус не начисляется");
+        }
+
+        }
+
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+
 }
